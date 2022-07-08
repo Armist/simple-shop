887107dc8f2b0c451cdb81a5fcfe94f87193cea3
Armist <armistman@gmail.com>
2022-07-08 22:12:43 +0500

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { useAppDispatch, useAppSelector } from './hooks/redux';
+import { fetchUsers } from './store/reducers/ActionCreators';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const dispatch = useAppDispatch();
+  const { users } = useAppSelector((state) => state.userReducer);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
+  return <div className="App">{JSON.stringify(users, null, 2)}</div>;
 }
 
 export default App;
